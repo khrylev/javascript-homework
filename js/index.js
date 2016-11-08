cff521ca62c216d95ff596bdd8f3d2a37611f316
Khrystyna Levytska <chrystialev@gmail.com>
2016-11-08 16:27:15 +0000

    a

--- js/index.js
+++ js/index.js
@@ -4,17 +4,18 @@ var a = Number(prompt('Input a'));
 var b = Number(prompt('Input b'));
 var c = Number(prompt('Input c'));
 
-var discr = b * b - 4 * a * c;
+var discr = (Math.pow(b, 2)) - 4 * a * c;
 var sqrDiscr = Math.sqrt(discr);
 
 function tellx (discr) {
- if (d = 0) {
-   return 'there is one root, x1 = x2 = -b / (2 * a)';
+ if (d == 0) {
+   return 'there is one root';
   }
   
  else {
   if (d < 0){
-  	result = 'there are no real roots, there are 2 complex roots: x1 = (-b + i√ - Δ) / (2 * a) and x2 = (-b - i√ - Δ)/(2 * a)';
+  	var x1 = (-b + i√ - Δ) / (2 * a);
+  	var x2 = (-b - i√ - Δ) / (2 * a);
   }
   
   if (d > 0) {
@@ -23,7 +24,4 @@ function tellx (discr) {
      
      result = 'x1 = ' + x1 + ', x2 = ' + x2;
  }
- 
  document.write(result);
-}
-
