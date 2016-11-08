c918d01a593b9f6f90267b06ff961276cfcccfd1
Khrystyna Levytska <chrystialev@gmail.com>
2016-11-08 22:18:57 +0000

    a

--- js/index.js
+++ js/index.js
@@ -1,26 +1,29 @@
-function calculate (){
+calcQuad();
 
-var a = Number(prompt('Input a'));
-var b = Number(prompt('Input b'));
-var c = Number(prompt('Input c'));
+    var a = Number(prompt('Input a'));
+    var b = Number(prompt('Input b'));
+    var c = Number(prompt('Input c'));
+    
+    var discr = (Math.pow(b, 2))- 4 * a * c;
+    
+    function calcQuad(x){
+    if (discr === 0) {
+       result = 'there is only one root';
+    }
+    else if (discr < 0){
+        result = 'try again';
+    }
+    if ( discr > 0) {
+        var x1 = ( - b + Math.sqrt(discr))/ (2 * a);
+        var x2 = ( - b - Math.sqrt(discr))/ (2 * a);
+        
+        result = 'x1 = ' + x1 + ', x2 = ' + x2;
+    }
+    
+    //var discr = Number(prompt('Tell your number'));//
+    var result = calcQuad(x);
+    document.write(result);
+}
 
-var discr = (Math.pow(b, 2)) - 4 * a * c;
-var sqrDiscr = Math.sqrt(discr);
-
-function tellx (discr) {
- if (d == 0) {
-   return 'there is one root';
-  }
-  
- else {
-  if (d < 0){
-  	return 'try again'
-  }
-  
-  if (d > 0) {
-     var x1 = (-b + Math.sqrt(discriminant)) / (2 * a);
-     var x2 = (-b - Math.sqrt(discriminant)) / (2 * a);
-     
-     result = 'x1 = ' + x1 + ', x2 = ' + x2;
- }
- document.write(result);
+ 
+ 
