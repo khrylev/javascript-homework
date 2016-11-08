1cc871ef63d139500e78b2d77d279fe426a204a4
Khrystyna Levytska <chrystialev@gmail.com>
2016-11-08 13:51:37 +0000

    add

---
+++ index.js
@@ -0,0 +1,20 @@
+function calculate (){
+var a = Number(prompt (a = "enter value of a"));
+var b = Number(prompt (b = "enter value of b"));
+var c = Number(prompt (c = "enter value of c"));
+
+var discr = Math.pow(b * b) - 4 * (a * c);
+
+function tellx (discr) {
+ if (a == 0 && b == 0 && c == 0) {
+    return 'Something went wrong';
+  }
+  
+ if (discr < 0){
+  	result = 'Your discriminant' + discriminant + 'is less than 0'
+        
+  
+  var age = Number(prompt('Tell x'));
+  var result = tellx(number);
+  document.write(result);
+  
