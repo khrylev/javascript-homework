2e218ffff08806891b2ae5bae6b1d192d8b2bb5e
Khrystyna Levytska <chrystialev@gmail.com>
2016-11-08 16:18:27 +0000

    a

--- js/index.js
+++ js/index.js
@@ -1,27 +1,29 @@
 function calculate (){
 
-var a = Number(prompt('enter value of a'));
-var b = Number(prompt('enter value of b'));
-var c = Number(prompt('enter value of c'));
+var a = Number(prompt('Input a'));
+var b = Number(prompt('Input b'));
+var c = Number(prompt('Input c'));
 
-var discriminant = b * b - 4 * a * c;
+var discr = b * b - 4 * a * c;
 var sqrDiscr = Math.sqrt(discr);
 
-function tellx (discriminant) {
- if (discriminant = 0) {
-    result = 'there is one root, x1 = x2 = -b / (2 * a)';
+function tellx (discr) {
+ if (d = 0) {
+   return 'there is one root, x1 = x2 = -b / (2 * a)';
   }
   
  else {
-  if (discriminant < 0){
+  if (d < 0){
   	result = 'there are no real roots, there are 2 complex roots: x1 = (-b + i√ - Δ) / (2 * a) and x2 = (-b - i√ - Δ)/(2 * a)';
- }   else if (discriminant > 0) {
+  }
+  
+  if (d > 0) {
      var x1 = (-b + Math.sqrt(discriminant)) / (2 * a);
      var x2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+     
+     result = 'x1 = ' + x1 + ', x2 = ' + x2;
  }
  
- var discr = Number(prompt('Tell a number'));
- var result = tellx;
  document.write(result);
-
+}
 
